3d52bb431842de90808732dd20760582a9c576e1
Tuan Pham 🐟 <tunaphishy37@gmail.com>
2024-11-12 13:05:38 -0500

    create layout for 4000 weeks grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,38 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+const Grid = () => {
+  const rows = 77;
+  const columns = 52;
+
+  // Calculate the width and height of each rectangle
+  const cellWidth = 100 / columns + 'vw';
+  const cellHeight = 100 / rows + 'vh';
+
+  // Generate the grid
+  const grid = [];
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < columns; col++) {
+      grid.push(
+        <div
+          key={`${row}-${col}`}
+          className="cell"
+        />
+      );
+    }
+  }
+
+  return <div className="grid">{grid}</div>;
+};
+
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
-
   return (
     <Layout location={location} title={siteTitle}>
+      {/* <blockquote>... what you pay attention to will define, for you, what reality is.</blockquote>
+      <Grid/> */}
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
